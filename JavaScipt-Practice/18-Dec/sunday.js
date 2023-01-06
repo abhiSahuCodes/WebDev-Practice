c915f5e90c768fa1c5ec4bc45d3eac32d5326c52
Abhishek Sahu <abhishek963sahu@gmail.com>
2023-01-06 23:04:31 +0530

    js function practice starting level

---
+++ JavaScipt-Practice/18-Dec/sunday.js
@@ -0,0 +1,90 @@
+// function sum(v1, v2){
+    
+//     console.log(`The sum of the numbers are ${v1+v2}`)
+// }
+
+
+// sum(21, 58);
+// sum(22, 58);
+// sum(23, 58);
+// sum(24, 58);
+// sum(25, 58);
+// sum(26, 58);
+// sum(27, 58);
+
+
+
+// function sum(val1, val2){
+//     let result = val1 + val2;
+//     return result;
+// }
+
+// let add = sum(10, 35);
+// console.log(add);
+
+// let add1 = sum(10, 43);
+// console.log(add1);
+
+
+//! node sunday.js
+
+// String
+
+// function URL(url, domain){
+//     let con = 'https://';
+//     let result = con + url + domain;
+//     return result;
+// }
+
+// let abhisheksite = URL('abhisheksahu', '.com');
+// console.log(abhisheksite);
+
+// Sum function  ========= node sunday.js
+
+// function sum(arr) {
+//     let sum = 0;
+//     for(let i = 0; i < arr.length; i++){
+//         sum = sum + arr[i];
+//     }
+//     return sum;
+// }    
+// let sumarray = [1, 2, 3, 4, 5, 6];
+// let arrresult = sum(sumarray);
+
+// console.log(arrresult);
+
+// ! Same sum function without using array
+
+// function abhishek(){
+    //^ In the blank parenthesis there is 'arguments' inside.
+    //^ console.log(arguments);
+//     let sum = 0;
+//     for (let i = 0; i < arguments.length; i++){
+//         sum = sum + arguments[i];
+//     }
+//     return sum;
+// }
+
+// console.log(abhishek(2, 3, 45, 34));
+
+// !--------Arrow in function---------------
+
+// function sudeshna(){
+//     console.log('Mein yahaan hoon');
+// }  This is a simple function and this can also be written with arrow method as:
+
+
+// let sudeshna = () => {
+//     console.log('Mein yahaan hoon');
+// };
+
+// sudeshna();
+// -------------------------------------
+// let himesh = (num1, num2) => {
+//     console.log('Tera tera tera suroor', num1, num2);
+// }
+
+// himesh(3, 4);
+// -------------------------------------
+// ! More programs----------------------
+
