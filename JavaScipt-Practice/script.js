4b1801b059657a394cbbed7874764c6b11c5e6a0
Abhishek Sahu <abhishek963sahu@gmail.com>
2023-01-06 15:21:26 +0530

    leap year code

--- JavaScipt-Practice/script.js
+++ JavaScipt-Practice/script.js
@@ -20,19 +20,35 @@
 
 //! ----To write a program to get the currrent date
 
-var today = new Date();
-var dd = today.getDate();
+// var today = new Date();
+// var dd = today.getDate();
 
-var mm = today.getMonth()+1;
-var yyyy = today.getFullYear();
-if (dd<10){
-    dd = '0' +dd;
+// var mm = today.getMonth()+1;
+// var yyyy = today.getFullYear();
+// if (dd<10){
+//     dd = '0' +dd;
+// }
+
+// if(mm<10){
+//     dd= '0' +mm;
+// }
+
+// today = dd+'-'+mm+'-'+yyyy;
+// console.log(today);
+
+//! This progarm is giving the month and year correctly but the day is incorrect. I think there is problem in '.getDate()'------But I am saving this code for further update.
+
+// ============================================
+// ! To find the leap year
+
+const year = prompt("Enter the year");
+
+console.log(year);
+
+if(year % 4 === 0){
+    console.log("Hey! This is a leap year.")
 }
-
-if(mm<10){
-    dd= '0' +mm;
+else{
+    console.log("This is not a leap year. Reload and try again!")
 }
-
-today = dd+'-'+mm+'-'+yyyy;
-console.log(today);
 
