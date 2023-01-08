7972b4c8e55327de4d48ce37d08f2f5699025862
Abhishek Sahu <abhishek963sahu@gmail.com>
2023-01-08 23:32:50 +0530

    days between days finding

--- JavaScipt-Practice/3rd-JS/script.js
+++ JavaScipt-Practice/3rd-JS/script.js
@@ -1,20 +1,20 @@
 // !--------Problem-1---------- //
 // Number of vowels in a input string
 
-let input = prompt("Enter the word or words");
+// let input = prompt("Enter the word or words");
 
-function getVowels(string){
-    var Vowels = ('AaEeIiOoUu');
-    var vowelsCount = 0;
-    for (var i = 0; i < string.length; i++){
-        if (Vowels.indexOf(string[i]) !== -1) {
-            vowelsCount += 1;
-        }
-    }
-    return vowelsCount;
-}
+// function getVowels(string){
+//     var Vowels = ('AaEeIiOoUu');
+//     var vowelsCount = 0;
+//     for (var i = 0; i < string.length; i++){
+//         if (Vowels.indexOf(string[i]) !== -1) {
+//             vowelsCount += 1;
+//         }
+//     }
+//     return vowelsCount;
+// }
 
-console.log(`The total vowels count in the input is: ${getVowels(input)}`);
+// console.log(`The total vowels count in the input is: ${getVowels(input)}`);
 
 // -----------------------------------------------
 
@@ -35,3 +35,22 @@ console.log(`The total vowels count in the input is: ${getVowels(input)}`);
 
 // ---------------------------------------------------------
 
+// JavaScript program to illustrate 
+    // calculation of no. of days between two date 
+      
+    //~ To set two dates to two variables
+    var date1 = new Date("06/30/2019");
+    var date2 = new Date("07/30/2019");
+      
+    //^ To calculate the time difference of two dates
+    var Difference_In_Time = date2.getTime() - date1.getTime();
+      
+    //^ To calculate the no. of days between two dates
+    var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+      
+    //^To display the final no. of days (result)
+    console.log("Total number of days between dates  <br>"
+               + date1 + "<br> and <br>" 
+               + date2 + " is: <br> " 
+               + Difference_In_Days);
+      
