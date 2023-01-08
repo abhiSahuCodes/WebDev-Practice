a092b3e1ed1ff950c88afa15c6b4e0a1b0beed5d
Abhishek Sahu <abhishek963sahu@gmail.com>
2023-01-08 23:28:38 +0530

    added prompt in vowel program

--- JavaScipt-Practice/3rd-JS/script.js
+++ JavaScipt-Practice/3rd-JS/script.js
@@ -1,33 +1,37 @@
 // !--------Problem-1---------- //
 // Number of vowels in a input string
 
+let input = prompt("Enter the word or words");
 
-// function getVowels(string){
-//     var Vowels = ('AaEeIiOoUu');
-//     var vowelsCount = 0;
-//     for (var i = 0; i < string.length; i++){
-//         if (Vowels.indexOf(string[i]) !== -1) {
-//             vowelsCount += 1;
-//         }
-//     }
-//     return vowelsCount;
-// }
+function getVowels(string){
+    var Vowels = ('AaEeIiOoUu');
+    var vowelsCount = 0;
+    for (var i = 0; i < string.length; i++){
+        if (Vowels.indexOf(string[i]) !== -1) {
+            vowelsCount += 1;
+        }
+    }
+    return vowelsCount;
+}
 
-// console.log(`The total vowels count in the input is: ${getVowels("Hello Brother")}`);
+console.log(`The total vowels count in the input is: ${getVowels(input)}`);
 
 // -----------------------------------------------
 
 // Program to generate a multiplication table
 
 // take input from the user
-const number = parseInt(prompt('Enter an integer: '));
+// const number = parseInt(prompt('Enter an integer: '));
 
 //creating a multiplication table
-for(let i = 1; i <= 10; i++) {
+// for(let i = 1; i <= 10; i++) {
 
-    // multiply i with number
-    const result = i * number;
+    //^ multiply i with number
+    // const result = i * number;
 
-    // display the result
-    console.log(`${number} * ${i} = ${result}`);
-}
+    //^ display the result
+//     console.log(`${number} * ${i} = ${result}`);
+// }
+
+// ---------------------------------------------------------
+
