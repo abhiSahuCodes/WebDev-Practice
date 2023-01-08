42c6f76402062348c950d60761d598589f6364d2
Abhishek Sahu <abhishek963sahu@gmail.com>
2023-01-08 23:18:02 +0530

    multiplication table

--- JavaScipt-Practice/3rd-JS/script.js
+++ JavaScipt-Practice/3rd-JS/script.js
@@ -2,17 +2,32 @@
 // Number of vowels in a input string
 
 
-let input = prompt("enter");
-function getVowels(string){
-    var Vowels = ('AaEeIiOoUu');
-    var vowelsCount = 0;
-    for (var i = 0; i < string.length; i++){
-        if (Vowels.indexOf(string[i]) !== -1) {
-            vowelsCount += 1;
-        }
-    }
-    return vowelsCount;
+// function getVowels(string){
+//     var Vowels = ('AaEeIiOoUu');
+//     var vowelsCount = 0;
+//     for (var i = 0; i < string.length; i++){
+//         if (Vowels.indexOf(string[i]) !== -1) {
+//             vowelsCount += 1;
+//         }
+//     }
+//     return vowelsCount;
+// }
+
+// console.log(`The total vowels count in the input is: ${getVowels("Hello Brother")}`);
+
+// -----------------------------------------------
+
+// Program to generate a multiplication table
+
+// take input from the user
+const number = parseInt(prompt('Enter an integer: '));
+
+//creating a multiplication table
+for(let i = 1; i <= 10; i++) {
+
+    // multiply i with number
+    const result = i * number;
+
+    // display the result
+    console.log(`${number} * ${i} = ${result}`);
 }
-
-console.log(`The total vowels count in the input is: ${getVowels("Hello Brother")}`);
-
