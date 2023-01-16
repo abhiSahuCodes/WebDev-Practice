5d53de9f373f1c7e7fef2b94f55aafb59821dcef
Abhishek Sahu <abhishek963sahu@gmail.com>
2023-01-16 21:01:46 +0530

    lexical scope is being practiced

--- JavaScipt-Practice/5th-JS/myScope/scope.js
+++ JavaScipt-Practice/5th-JS/myScope/scope.js
@@ -54,10 +54,50 @@
 // console.log(msg)
 //^ It will only print radius as the value of variable radius is defined outside the scope. It won't print msg.
 
-for (let i = 0; i < 5; i++) {
-    let msg = "ASKLDJAKLSJD";
-    console.log(msg)
+// for (let i = 0; i < 5; i++) {
+//     let msg = "ASKLDJAKLSJD";
+//     console.log(msg)
+// }
+// console.log(msg)  //^ The msg won't be printed as it is outside. It can only be printed if the let is replaced with var.
+// console.log(i) //^ This i can't be printed as the it is defined inside the for loop. But it can be printed if let i = 0 is replaced by var i = 0. 
+
+//& ==============Lexical Scope==================
+
+//^ An inner child function inside a parent function can access the variables defined outside that child function which is defined inside the parent function.
+
+// function bankRobbery() {
+//     const heros = ['Shaktiman', 'Superman', 'Batman'];
+//     function cryForHelp() {
+//         for (let hero of heros) {
+//             console.log(`Please, come and help us ${hero.toUpperCase()}.`)
+//         }
+//     }
+//     cryForHelp();
+// }
+
+// bankRobbery();
+
+//^ This will print the console.log contents with required variable value. This shows that the console.log inside the cryForHelp can actually access the values inside the variable heros.
+
+
+//^ This also can be said if there are two levels i.e, parent, child, grandchild. 
+
+function bankRobbery() {
+    const heros = ['Shaktiman', 'Superman', 'Batman'];
+    //todo console.log(color); 
+    //^ This console.log(color) can't access what's inside its child and will give an error.
+    function cryForHelp() {
+        let color = 'milkygray';
+        function grandChild() {
+            for (let hero of heros) {
+                console.log(`Please, come and help us ${hero.toUpperCase()}.`)
+                console.log(color); //^ This can be accessed by grandChild.
+            }
+        }
+        grandChild();
+    }
+    cryForHelp();
 }
-console.log(msg)  //^ The msg won't be printed as it is outside. It can only be printed if the let is replaced with var.
-console.log(i) //^ This i can't be printed as the it is defined inside the for loop. But it can be printed if let i = 0 is replaced by var i = 0. 
+
+bankRobbery();
 
