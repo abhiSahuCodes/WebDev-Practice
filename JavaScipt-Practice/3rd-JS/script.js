5a939b91e0addd18aaee17903e0e1af494f14559
Abhishek Sahu <abhishek963sahu@gmail.com>
2023-01-22 23:13:05 +0530

    to get volume of a cylinder

--- JavaScipt-Practice/3rd-JS/script.js
+++ JavaScipt-Practice/3rd-JS/script.js
@@ -398,25 +398,41 @@ let movies = ['Hum Tum', 'Hum Dil De Chuke Sanam', 'Hasina Maan Jayegi']
 
 //^ Javascript program to count keys in an object
 
-const student = { 
-    name: 'Roshan',
-    age: 25,
-    hobbies: ['dancing', 'cooking', 'singing'],
-    address: 'Bargarh'
-};
+// const student = { 
+//     name: 'Roshan',
+//     age: 25,
+//     hobbies: ['dancing', 'cooking', 'singing'],
+//     address: 'Bargarh'
+// };
 
-let count = 0;
+// let count = 0;
 
-// loop through each key/value
-for(let key in student) {
+// //^ loop through each key/value
+// for(let key in student) {
 
-    // increase the count
-    ++count;
+//     //^ increase the count
+//     ++count;
+// }
+
+// console.log(count);
+
+// !-------------------------------------------------------------------
+
+//^ Program to find the volume of a cylinder
+
+function Cylinder(cyl_height, cyl_diameter) {
+  this.cyl_height = cyl_height;
+  this.cyl_diameter = cyl_diameter;
 }
 
-console.log(count);
+Cylinder.prototype.Volume = function () {
+  var radius = this.cyl_diameter / 2;
+  return this.cyl_height * Math.PI * radius * radius;
+};
 
-
+var cyl = new Cylinder(7, 4);
+// Volume of the cylinder with four decimal places.
+console.log("volume =", cyl.Volume().toFixed(4));
 
 
 
