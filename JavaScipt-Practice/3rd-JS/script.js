9d795f1bcdd424096ed05187dff558f64f41ab64
Abhishek Sahu <abhishek963sahu@gmail.com>
2023-01-22 23:19:40 +0530

    area and perimeter of a circle program

--- JavaScipt-Practice/3rd-JS/script.js
+++ JavaScipt-Practice/3rd-JS/script.js
@@ -395,7 +395,7 @@ let movies = ['Hum Tum', 'Hum Dil De Chuke Sanam', 'Hasina Maan Jayegi']
 // console.log(result);
 
 // !--------------------------------------------------------------------------------------
-
+//& Revise from here important 
 //^ Javascript program to count keys in an object
 
 // const student = { 
@@ -420,19 +420,37 @@ let movies = ['Hum Tum', 'Hum Dil De Chuke Sanam', 'Hasina Maan Jayegi']
 
 //^ Program to find the volume of a cylinder
 
-function Cylinder(cyl_height, cyl_diameter) {
-  this.cyl_height = cyl_height;
-  this.cyl_diameter = cyl_diameter;
+// function Cylinder(cyl_height, cyl_diameter) {
+//   this.cyl_height = cyl_height;
+//   this.cyl_diameter = cyl_diameter;
+// }
+
+// Cylinder.prototype.Volume = function () {
+//   var radius = this.cyl_diameter / 2;
+//   return this.cyl_height * Math.PI * radius * radius;
+// };
+
+// var cyl = new Cylinder(7, 4);
+// //^ Volume of the cylinder with four decimal places.
+// console.log("volume =", cyl.Volume().toFixed(4));
+
+// !-------------------------------------------------------------------
+//^ To find the area and perimeter of a circle
+ 
+function circle(radius) {
+  this.radius = radius;
+  //^ area method
+  this.area = function () {
+    return Math.PI * this.radius * this.radius;
+  };
+  //^ perimeter method
+  this.perimeter = function () {
+    return 2 * Math.PI * this.radius;
+  };
 }
-
-Cylinder.prototype.Volume = function () {
-  var radius = this.cyl_diameter / 2;
-  return this.cyl_height * Math.PI * radius * radius;
-};
-
-var cyl = new Cylinder(7, 4);
-// Volume of the cylinder with four decimal places.
-console.log("volume =", cyl.Volume().toFixed(4));
+var c = new circle(3);
+console.log("Area =", c.area().toFixed(2));
+console.log("perimeter =", c.perimeter().toFixed(2));
 
 
 
