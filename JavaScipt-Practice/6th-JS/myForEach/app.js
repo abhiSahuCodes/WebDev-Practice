35468046646abd4a4207e1ead07178a6f5e30b5f
Abhishek Sahu <abhishek963sahu@gmail.com>
2023-01-27 23:06:01 +0530

    more forEach practice

--- JavaScipt-Practice/6th-JS/myForEach/app.js
+++ JavaScipt-Practice/6th-JS/myForEach/app.js
@@ -26,8 +26,35 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 
 // }
 
 //^ Now lets print the even numbers only
-numbers.forEach(function(el) {
-    if (el % 2 === 0) {
-        console.log(el);
+// numbers.forEach(function(el) {
+//     if (el % 2 === 0) {
+//         console.log(el);
+//     }
+// })
+
+//^ Another example of forEach use
+const movies = [
+    {
+        title: 'Amadeus',
+        score: 99,
+    },
+    {
+        title: 'Stand By Me',
+        score: 85,
+    },
+    {
+        title: 'Parasite',
+        score: 95,
+    },
+    {
+        title: 'Alien',
+        score: 90,
     }
+]    
+
+//^ We want to print somethings like e.g.'Alien - 90/100'
+//^ This is how we do it
+
+movies.forEach(function(movie){
+    console.log(`${movie.title}- ${movie.score}/100`);
 })
