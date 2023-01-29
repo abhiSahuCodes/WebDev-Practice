83cc759af77b1efa6a3a5e7ee022b74a998f8376
Abhishek Sahu <abhishek963sahu@gmail.com>
2023-01-29 23:57:02 +0530

    another arrow function example with regular, arrow, and implicit return syntax where map is used

--- JavaScipt-Practice/6th-JS/myArrowFunctions/app.js
+++ JavaScipt-Practice/6th-JS/myArrowFunctions/app.js
@@ -39,14 +39,56 @@
 
 //! Implicit Return (Only works if there is only one line of code inside the function)
 //^ The same arrow function can also be written without return keyword as well as without curly braces using simple parenthesis
-let rollDie = () => (
-    Math.floor(Math.random() * 6) +1
-) 
+// let rollDie = () => (
+//     Math.floor(Math.random() * 6) +1
+// ) 
 
-console.log(rollDie());
+// console.log(rollDie());
 
 //^ This can also be written without any parenthesis in one line
-let rollDieAgain = () => Math.floor(Math.random() * 6) +1;
-console.log(rollDieAgain());
+// let rollDieAgain = () => Math.floor(Math.random() * 6) +1;
+// console.log(rollDieAgain());
 
+//^ Another example of arrow function
 
+const movies = [
+    {
+      title: "Amadeus",
+      score: 99,
+    },
+    {
+      title: "Stand By Me",
+      score: 85,
+    },
+    {
+      title: "Parasite",
+      score: 95,
+    },
+    {
+      title: "Alien",
+      score: 90,
+    },
+];
+
+//^ For the map method we used to write
+
+// const outOfTen = movies.map(function (movieName) {
+//     return `${movieName.title} has got ${movieName.score / 10}`
+// });
+
+// console.log(outOfTen);
+
+//^ With arrow method
+// const outOfTen = movies.map(movieName => {
+//     return `${movieName.title} has got ${movieName.score / 10}`
+// })
+
+// console.log(outOfTen);
+
+//^ Implicit Return
+
+const outOfTen = movies.map(movieName => (
+    `${movieName.title} has got ${movieName.score / 10}`
+))
+
+console.log(outOfTen);
