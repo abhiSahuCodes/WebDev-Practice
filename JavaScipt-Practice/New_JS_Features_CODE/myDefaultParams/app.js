2618d7e6d1a2896425bca9ddf955abe97945c573
Abhishek Sahu <abhishek963sahu@gmail.com>
2023-02-01 01:00:36 +0530

    more default param practice

--- JavaScipt-Practice/New_JS_Features_CODE/myDefaultParams/app.js
+++ JavaScipt-Practice/New_JS_Features_CODE/myDefaultParams/app.js
@@ -14,7 +14,23 @@
 // } //^ This will take numSides as 6 if the value is not given
 
 //^ New way of defining parameter
-function rollDie(numSides = 6) {
-    return Math.floor(Math.random() * numSides) + 1;
-}
-console.log(rollDie());
+// function rollDie(numSides = 6) {
+//     return Math.floor(Math.random() * numSides) + 1;
+// }
+// console.log(rollDie());
+
+//^ Greet function with default param (using two parameter and putting a default value in one parameter)
+// function greet(msg, person) {
+//     return `${msg} ${person}`;
+// }
+
+//^ Now passing a default param
+// function greet(msg = "Hey", person) {
+//     return `${msg} ${person}`; //^ This will give an error (David undefined) on putting one value as it will interfere with "Hey" of msg as it is the first value that shouldnot be put as first parameter
+// }
+
+function greet(person, msg ="Hey", punc = "!!") {
+    return `${msg} ${person} ${punc}`
+} //^ This will work if given only one parameter while calling
+
+console.log(greet("Mark"));
