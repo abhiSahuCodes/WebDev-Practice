db5a619f5aa43a9413a9480a85da2ce231a4ce4b
Abhishek Sahu <abhishek963sahu@gmail.com>
2023-02-01 00:08:43 +0530

    more this inside arrow function testing

--- JavaScipt-Practice/6th-JS/myArrowFunctionsThis/app.js
+++ JavaScipt-Practice/6th-JS/myArrowFunctionsThis/app.js
@@ -8,11 +8,42 @@
 // }
 // console.log(person.fullName()); //& Abhishek Sahu
 
+// const person = {
+//   firstName: "Abhishek",
+//   lastName: "Sahu",
+//   fullName: () => {
+//     return `${this.firstName} ${this.lastName}`;
+//   }, //^ Here this keyword is limited to the scope.
+// };
+// console.log(person.fullName()); //& It will show error (undefined undefined)
+
+// const person = {
+//   firstName: "Abhishek",
+//   lastName: "Sahu",
+//   fullName: function () {
+//     return `${this.firstName} ${this.lastName}`;
+//   },
+//   shoutName: function () {
+//         setTimeout(() => {
+//             //^ keyword 'this' in arrow functions refers to the value of 'this' when the function is created
+//             console.log(this);
+//             console.log(this.fullName())
+//         }, 3000)
+//     }
+// };
+
+//^ The above will work : shoutName: this will show the whole person and this.fullName will show "Abhishek Sahu" after 3secs
+
 const person = {
   firstName: "Abhishek",
   lastName: "Sahu",
-  fullName: () => {
+  fullName: function () {
     return `${this.firstName} ${this.lastName}`;
-  }, //^ Here this keyword is limited to the scope.
-};
-console.log(person.fullName()); //& It will show error (undefined undefined)
+  },
+  shoutName: function () {
+    setTimeout(function () {
+      console.log(this);
+      console.log(this.fullName());
+    }, 3000);
+  },
+}; //^ Here the this will show whole person but this.fullName will give error
