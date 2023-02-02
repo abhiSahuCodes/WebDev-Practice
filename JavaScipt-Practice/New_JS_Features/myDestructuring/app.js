1b47220fd6bb39f49b0d40937b67727992c74de0
Abhishek Sahu <abhishek963sahu@gmail.com>
2023-02-02 23:37:31 +0530

    destructuring param

--- JavaScipt-Practice/New_JS_Features/myDestructuring/app.js
+++ JavaScipt-Practice/New_JS_Features/myDestructuring/app.js
@@ -44,14 +44,92 @@ const user = {
 // const {born: birthYear, died: deathYear = "N/A"} = user
 // console.log({born: birthYear, died: deathYear: "N/A"});
 //^ But we can actually add a key and it's value if not present
-const user2 = {
-  email: "Stacy@gmail.com",
-  firstName: "Stacy",
-  lastName: "Gonzalez",
-  born: 1987,
-  city: "Tulsa",
-  state: "Oklahoma",
-};
+// const user2 = {
+//   email: "Stacy@gmail.com",
+//   firstName: "Stacy",
+//   lastName: "Gonzalez",
+//   born: 1987,
+//   city: "Tulsa",
+//   state: "Oklahoma",
+// };
 
-const {city, state, died = 'N/A'} = user2
-console.log({city, state, died : 'N/A'})
+// const {city, state, died = 'N/A'} = user2
+// console.log({city, state, died : 'N/A'})
+
+//!-----PARAM DESTRUCTURING-----
+//^ Here we are using user2 object for this function
+// function fullName(user2) {
+//     return `${user2.firstName} ${user2.lastName}`
+// }  
+// console.log(fullName(user2));
+
+//^ We can destructure the above and can write it as:
+// function fullName(user2) {
+//     const {firstName, lastName} = user2;
+//     return `${firstName} ${lastName}`
+// } 
+// console.log(fullName(user2));
+
+const movies = [
+  {
+    title: "Amadeus",
+    score: 99,
+    year: 1984,
+  },
+  {
+    title: "Sharknado",
+    score: 35,
+    year: 2013,
+  },
+  {
+    title: "13 Going On 30",
+    score: 70,
+    year: 2004,
+  },
+  {
+    title: "Stand By Me",
+    score: 85,
+    year: 1986,
+  },
+  {
+    title: "Waterworld",
+    score: 62,
+    year: 1995,
+  },
+  {
+    title: "Jingle All The Way",
+    score: 71,
+    year: 1996,
+  },
+  {
+    title: "Parasite",
+    score: 95,
+    year: 2019,
+  },
+  {
+    title: "Notting Hill",
+    score: 77,
+    year: 1999,
+  },
+  {
+    title: "Alien",
+    score: 90,
+    year: 1979,
+  },
+];
+
+//^ To filter out only the movies with scores above 90
+// const aboveNinety = movies.filter((movie) => movie.score >= 90);
+//^ By destructuring this also can be written as 
+// const aboveNinety = movies.filter(({score}) => score >= 90);
+// console.log(aboveNinety);  
+
+//^ Little complicated use 
+// const movieByYear = movies.map((movie) => `${movie.title} released in ${movie.year} has got ${movie.score}.`)
+
+// console.log(movieByYear);
+
+//^ Upon destructuring the above can be written as:
+const movieByYear = movies.map(({title, score, year}) => `${title} released in ${year} has got ${score}.`)
+
+console.log(movieByYear);
