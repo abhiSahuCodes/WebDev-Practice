7529dc8adb1ff2a77ce51bf9fc55a14d625caeb5
Abhishek Sahu <abhishek963sahu@gmail.com>
2023-02-04 23:14:57 +0530

    JS Dates practice

--- JavaScipt-Practice/JavaScript-Miscellaneous-Practice/1-one-JS/app.js
+++ JavaScipt-Practice/JavaScript-Miscellaneous-Practice/1-one-JS/app.js
@@ -1,4 +1,6 @@
+//===============================
 // ! ----JS-eval---- //
+//===============================
 
 //^ The eval() function in JavaScript is used to evaluate the expression. It is JavaScirpt's global function, which evaluates the specified string as JavaScript code and executes it.
 
@@ -19,6 +21,26 @@ let str = '({"firstName" : "Abhishek", "lastName" : "Sahu"})';
 let obj = eval(str);
 
 console.log(obj.firstName + " " + obj.lastName);
+
+//===============================
+// ! ----JS-Dates---- //
+//===============================
+
+//Example-1 Current Date
+
+let date = new Date();
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+console.log("Date is: " + day + "/" + month + "/" + year);  
+
+//Example-2 
+
+let today = new Date();
+let h = today.getHours();
+let m = today.getMinutes();
+let s = today.getSeconds();
+console.log (h + ":" + m + ":" + s);
 
 
 
