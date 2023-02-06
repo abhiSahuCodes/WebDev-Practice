d21c8f5510cdde504b097d6086be2f5e1ec34348
Abhishek Sahu <abhishek963sahu@gmail.com>
2023-02-07 00:00:07 +0530

    copyWithin

--- JavaScipt-Practice/3rd-JS/script.js
+++ JavaScipt-Practice/3rd-JS/script.js
@@ -437,20 +437,31 @@ let movies = ['Hum Tum', 'Hum Dil De Chuke Sanam', 'Hasina Maan Jayegi']
 // !-------------------------------------------------------------------
 //^ To find the area and perimeter of a circle
  
-function circle(radius) {
-  this.radius = radius;
-  //^ area method
-  this.area = function () {
-    return Math.PI * this.radius * this.radius;
-  };
-  //^ perimeter method
-  this.perimeter = function () {
-    return 2 * Math.PI * this.radius;
-  };
-}
-var c = new circle(3);
-console.log("Area =", c.area().toFixed(2));
-console.log("perimeter =", c.perimeter().toFixed(2));
+// function circle(radius) {
+//   this.radius = radius;
+//   //^ area method
+//   this.area = function () {
+//     return Math.PI * this.radius * this.radius;
+//   };
+//   //^ perimeter method
+//   this.perimeter = function () {
+//     return 2 * Math.PI * this.radius;
+//   };
+// }
+// var c = new circle(3);
+// console.log("Area =", c.area().toFixed(2));
+// console.log("perimeter =", c.perimeter().toFixed(2));
+
+// !-------------------------------------------------------------------
+
+//^ Array.prototype.copyWithin()
+
+//todo-------method/syntex----------> Array.prototype.copyWithin(target : number, start : number, end = this.length) : This
+
+//It copies the elements whose indices are in the range [start,end) to index target and subsequent indices. If the two index ranges overlap, care is taken that all source elements are copied before they are overwritten.
+
+const arr = ["a", "b", "c", "d", "e"];
+console.log(arr.copyWithin(1, 3));
 
 
 
