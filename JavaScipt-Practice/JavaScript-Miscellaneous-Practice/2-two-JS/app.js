b189252b51fa3cc470ef96d2160de73ea14ebb7c
Abhishek Sahu <abhishek963sahu@gmail.com>
2023-02-06 22:26:49 +0530

    for loop inside a function and same array printing with forEach and map method

--- JavaScipt-Practice/JavaScript-Miscellaneous-Practice/2-two-JS/app.js
+++ JavaScipt-Practice/JavaScript-Miscellaneous-Practice/2-two-JS/app.js
@@ -6,17 +6,41 @@
 
 //^ Nested loop 
 
-let europeCountry = [
-  ["Spain", "Germany", "England"],
-  ["Madrid", "Berlin", "London"],
-  ["Jordi Albas Ramos", 'Manuel Neuer', 'Harry Cane'],
-];
+// let europeCountry = [
+//   ["Spain", "Germany", "England"],
+//   ["Madrid", "Berlin", "London"],
+//   ["Jordi Albas Ramos", 'Manuel Neuer', 'Harry Cane'],
+// ];
 
-for (let i = 0; i < europeCountry.length; i++) {
-    const countryGame = europeCountry[i];
-    console.log(`Row #${i}`);
-    for (let j = 0; j < countryGame.length; j++) {
-        console.log(countryGame[j]);
-    }
-}
+// for (let i = 0; i < europeCountry.length; i++) {
+//     const countryGame = europeCountry[i];
+//     console.log(`Row #${i}`);
+//     for (let j = 0; j < countryGame.length; j++) {
+//         console.log(countryGame[j]);
+//     }
+// }
 
+//^ Iterating over array
+
+const birdsIndia = [
+  'Indian Roller',
+  'Asian Koel',
+  'Grey Junglefowl',
+  'Red Knot',
+  'Grey Heron',
+  'Greater Flamingo',
+  'Hornbill'
+]
+
+// function birds() { 
+//     for (let i = 0; i < birdsIndia.length; i++) {
+//     console.log(i + 1, birdsIndia[i]); //To print the names along with numbering
+//   }
+// }
+
+// birds();
+
+// let listBirds = birdsIndia.forEach(birds => console.log(birds)); //using arrow with implicit
+// console.log(listBirds);
+
+let listBirds = birdsIndia.map((birds) => console.log(birds));
