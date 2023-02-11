f32014cf143cafe081a167a883de7424120ef43f
Abhishek Sahu <abhishek963sahu@gmail.com>
2023-02-11 23:35:34 +0530

    parseFloat and parseInt

---
+++ JavaScipt-Practice/JavaScript-Miscellaneous-Practice/3-three-js/app.js
@@ -0,0 +1,8 @@
+// !==============JavaScript Functions===============
+//^ JavaScript parsFloat
+let p1 = parseFloat("18.147656");
+console.log('Using parseFloat("18.147656") = ' + p1);
+
+//^ JavaScript parsInt
+var p2 = parseInt("18great");
+console.log(`${p2}`);
