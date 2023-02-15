c30e0b42cace7e9dc2c24db871073c94eb132395
Abhishek Sahu <abhishek963sahu@gmail.com>
2023-02-15 21:50:04 +0530

    change and input events in form using js DOM

---
+++ JavaScipt-Practice/DOM_Events_CODE/myCodes/jsChangeAndInputEvents/app.js
@@ -0,0 +1,18 @@
+const input = document.querySelector('input');
+const h1 = document.querySelector('h1');
+// input.addEventListener('change', function (e) {
+//     console.log("CHANGE HAPPENED!!!")
+// })
+
+//^ Changes the h1 after changing input and hitting enter 
+// input.addEventListener('change', function (e) {
+//     h1.innerText = input.value;
+// }) 
+
+
+//^ Live change in h1 as the user type 
+input.addEventListener("input", function (e) {
+  h1.innerText = input.value;
+});
+
+
